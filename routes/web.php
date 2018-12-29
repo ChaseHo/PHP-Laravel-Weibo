3070284c51b834681c7a80c2e769f9e0bece5939
ChaseHo <842416587@qq.com>
2018-12-29 06:54:36 +0000

    Move user model to models folder

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+//路由的注册
 Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
