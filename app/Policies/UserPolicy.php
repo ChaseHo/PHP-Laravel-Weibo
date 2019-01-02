b0bcb5d0cd8c64319103e2863be2ac09e6264915
ChaseHo <842416587@qq.com>
2019-01-02 06:45:45 +0000

    管理员可删除用户

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -2,25 +2,21 @@
 
 namespace App\Policies;
 
-use App\Models\User;
 use Illuminate\Auth\Access\HandlesAuthorization;
+use App\Models\User;
 
 class UserPolicy
 {
     use HandlesAuthorization;
 
-    /**
-     * Create a new policy instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        //
-    }
-
     public function update(User $currentUser, User $user)
     {
         return $currentUser->id === $user->id;
+    }
+
+    public function destroy(User $currentUser, User $user)
+    {
+        //至有当前用户拥有管理员权限且删除的用户不是自己的时候才显示链接
+        return $currentUser->is_admin && $currentUser->id !== $user->id;
     }
 }
