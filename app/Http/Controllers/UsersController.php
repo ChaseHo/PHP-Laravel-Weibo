384402d70638512789397e07dd0bd42862aad91c
ChaseHo <842416587@qq.com>
2019-01-02 10:13:43 +0000

    微博数据模型

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -102,25 +102,25 @@ class UsersController extends Controller
     //发送邮箱验证
     protected function sendEmailConfirmationTo($user)
     {
-        // $view = 'emails.confirm';
-        // $data = compact('user');
-        // $from = 'summer@example.com';
-        // $name = 'Summer';
-        // $to = $user->email;
-        // $subject = "感谢注册 Weibo 应用！请确认你的邮箱。";
-
-        // Mail::send($view, $data, function ($message) use ($from, $name, $to, $subject) {
-        //     $message->from($from, $name)->to($to)->subject($subject);
-        // });
-
         $view = 'emails.confirm';
         $data = compact('user');
+        $from = '641037187@qq.com';
+        $name = '641037187@qq.com';
         $to = $user->email;
         $subject = "感谢注册 Weibo 应用！请确认你的邮箱。";
 
-        Mail::send($view, $data, function ($message) use ($to, $subject) {
-            $message->to($to)->subject($subject);
+        Mail::send($view, $data, function ($message) use ($from, $name, $to, $subject) {
+            $message->from($from, $name)->to($to)->subject($subject);
         });
+
+        // $view = 'emails.confirm';
+        // $data = compact('user');
+        // $to = $user->email;
+        // $subject = "感谢注册 Weibo 应用！请确认你的邮箱。";
+
+        // Mail::send($view, $data, function ($message) use ($to, $subject) {
+        //     $message->to($to)->subject($subject);
+        // });
     }
 
     //邮箱激活成功的函数
